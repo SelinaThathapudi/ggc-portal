cb2e68a0b0471399ea554ecc5f4a5f373c15d5ea
SelinaThathapudi <selinathathapudi@gmail.com>
2024-01-19 22:16:41 -0600

    first iteration complete

--- ggc.portal.client/src/app-shell/header-title/header-title.component.ts
+++ ggc.portal.client/src/app-shell/header-title/header-title.component.ts
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./header-title.component.scss']
 })
 export class HeaderTitleComponent {
+  public products: Array<any> = [
+    {
+      id: '1',
+      name: 'Home',
+    },
+    {
+      id: '4',
+      name: 'Location',
+    },
+    {
+      id: '2',
+      name: 'About us',
+    }
+    // {
+    //   id: '3',
+    //   name: 'Ministries',
+    //   items: [
+    //     {
+    //       id: '3_1',
+    //       name: "Kids' Ministry",
+    //     },
+    //     {
+    //       id: '3_2',
+    //       name: 'Youth Ministry',
+    //     },
+    //     {
+    //       id: '3_3',
+    //       name: "Women's Ministry",
+    //     },
+    //     {
+    //       id: '3_4',
+    //       name: "Men's Ministry",
+    //     },
+    //   ],
+    // },
+  ];
+
+  itemClick(data: any) {
+    console.log(data);
+  }
 
 }
